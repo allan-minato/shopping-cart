5cd95d5c8b97214d8e6f24102c49550d575d6336
Allan Medeiros Minato <allan.minato@gmail.com>
2022-11-18 02:55:44 -0300

    Resolução exercício 2

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,12 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = async (product) => {
+  if (!product) {
+    throw new Error('Termo de busca não informado');
+  }
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
+  const data = await fetch(url);
+  const response = await data.json();
+  return response.results;
 };
