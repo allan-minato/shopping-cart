7b070063d7975641dc2ca6067f3b9a7afdce3b0a
Allan Medeiros Minato <allan.minato@gmail.com>
2022-11-18 20:36:11 -0300

    Resolução do exercício 3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const productsList = await fetchProductsList('computador');
+const products = document.querySelector('.products');
+
+productsList.forEach((product) => {
+  products.appendChild(createProductElement(product));
+});
