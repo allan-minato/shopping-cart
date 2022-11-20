8c3e3b8d8b1610c9be1a5afae49c4760b8b7a7e1
Allan Medeiros Minato <allan.minato@gmail.com>
2022-11-20 19:53:06 -0300

    fix 6

--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -10,4 +10,13 @@ describe('Teste a função fetchProduct', () => {
   it('Teste "FetchProduct" com o parâmetro "MLB1405519561".', () => {
     expect(fetchProduct('MLB1405519561')).not.toBe(undefined)
   });
+  it("Teste se o retorno da função fetchProduct com o argumento do produto 'MLB1405519561' é uma estrutura de dados igual ao objeto produto que já está importado no arquivo.", async () => {
+		expect(await fetchProduct('MLB1405519561')).toEqual(product);
+	});
+
+	it("Teste se, ao chamar a função fetchProduct sem argumento, retorna um erro com a mensagem: 'ID não informado'", () => {
+		return fetchProduct().catch(error => {
+			expect(error.message).toBe('ID não informado')
+		})
+	});
 });
